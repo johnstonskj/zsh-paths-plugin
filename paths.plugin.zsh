984bf71891dc0bbf7300d66e76c13a088a1b5e88
Simon Johnston <johnstonskj@gmail.com>
2026-03-10 11:16:54 -0700

    style: new header doc formatting

--- paths.plugin.zsh
+++ paths.plugin.zsh
@@ -1,8 +1,10 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# Plugin Name: paths
-# Description: Simple functions for managing PATH, MANPATH and FPATH.
-# Repository: https://github.com/johnstonskj/zsh-paths-plugin
+# @name: paths
+# @brief: Simple functions for managing `PATH`, `MANPATH` and `FPATH`.
+# @repository: https://github.com/johnstonskj/zsh-paths-plugin
+# @version: 0.1.1
+# @license: MIT AND Apache-2.0
 #
 # Public variables:
 #
